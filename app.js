fe7738759201399ebffdc28d99763d8493b2975c
Linus Unnebäck <linus@folkdatorn.se>
2017-08-14 23:30:01 +0100

    🎉 Add progress indicators

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
+const ora = require('ora')
 const neodoc = require('neodoc')
 const parseArn = require('aws-arn-parser')
+const prettyBytes = require('pretty-bytes')
 
 const amazon = require('./lib/amazon')
 const builder = require('./lib/builder')
@@ -24,59 +26,68 @@ Options:
 
 async function main () {
   const args = neodoc.run(usage)
+  const spinner = ora()
 
-  if (args.create) {
-    const zipFile = await builder.createZipFile(process.cwd())
-    const result = await amazon.createFunction({ zipFile, functionName: args['<name>'], role: args['--role'] })
+  try {
+    if (args.create) {
+      spinner.start('Packaging app for Lambda')
+      const zipFile = await builder.createZipFile(process.cwd())
+      spinner.succeed(`App packaged successfully! Final size: ${prettyBytes(zipFile.byteLength)}`)
 
-    const lambdaArn = result.FunctionArn
+      spinner.start('Creating Lambda function')
+      const lambdaArn = await amazon.createFunction({ zipFile, functionName: args['<name>'], role: args['--role'] })
+      spinner.succeed(`Created new Lambda function with ARN: ${lambdaArn}`)
 
-    console.log(`Created new Lambda function with ARN: ${lambdaArn}`)
+      const definition = args['--swagger']
+        ? await swagger.loadSwaggerFile(args['--swagger'], lambdaArn)
+        : await swagger.forwardAllDefinition(lambdaArn)
 
-    const definition = args['--swagger']
-      ? await swagger.loadSwaggerFile(args['--swagger'], lambdaArn)
-      : await swagger.forwardAllDefinition(lambdaArn)
+      spinner.start('Creating new API Gateway')
+      const { id } = await amazon.createApiGateway({ definition, lambdaArn })
+      spinner.succeed(`Created new API Gateway with id: ${id}`)
 
-    const { id } = await amazon.createApiGateway({ definition, lambdaArn })
+      if (args['--deploy']) {
+        const stage = 'prod'
+        const { region } = parseArn(lambdaArn)
 
-    console.log(`Created new API Gateway with id: ${id}`)
-
-    if (args['--deploy']) {
-      const stage = 'prod'
-      const { region } = parseArn(lambdaArn)
-
-      await amazon.deployApiGateway({ id, stage })
-
-      console.log(`Now serving live requests at: https://${id}.execute-api.${region}.amazonaws.com/${stage}`)
+        spinner.start('Deploying the API to a live address')
+        await amazon.deployApiGateway({ id, stage })
+        spinner.succeed(`Now serving live requests at: https://${id}.execute-api.${region}.amazonaws.com/${stage}`)
+      }
     }
-  }
 
-  if (args.update) {
-    const zipFile = await builder.createZipFile(process.cwd())
-    const result = await amazon.updateFunction({ zipFile, functionName: args['<name>'] })
+    if (args.update) {
+      spinner.start('Packaging app for Lambda')
+      const zipFile = await builder.createZipFile(process.cwd())
+      spinner.succeed(`App packaged successfully! Final size: ${prettyBytes(zipFile.byteLength)}`)
 
-    const lambdaArn = result.FunctionArn
+      spinner.start('Creating Lambda function')
+      const lambdaArn = await amazon.updateFunction({ zipFile, functionName: args['<name>'] })
+      spinner.succeed(`Updated existing Lambda function with ARN: ${lambdaArn}`)
 
-    console.log(`Updated existing Lambda function with ARN: ${lambdaArn}`)
+      const definition = args['--swagger']
+        ? await swagger.loadSwaggerFile(args['--swagger'], lambdaArn)
+        : await swagger.forwardAllDefinition(lambdaArn)
 
-    const definition = args['--swagger']
-      ? await swagger.loadSwaggerFile(args['--swagger'], lambdaArn)
-      : await swagger.forwardAllDefinition(lambdaArn)
+      const id = args['--rest-api-id']
 
-    const id = args['--rest-api-id']
+      spinner.start('Updating existing API Gateway')
+      await amazon.updateApiGateway({ id, definition, lambdaArn })
+      spinner.succeed(`Updated existing API Gateway with id: ${id}`)
 
-    await amazon.updateApiGateway({ id, definition, lambdaArn })
+      if (args['--deploy']) {
+        const stage = 'prod'
+        const { region } = parseArn(lambdaArn)
 
-    console.log(`Updated existing API Gateway with id: ${id}`)
-
-    if (args['--deploy']) {
-      const stage = 'prod'
-      const { region } = parseArn(lambdaArn)
-
-      await amazon.deployApiGateway({ id, stage })
-
-      console.log(`Now serving live requests at: https://${id}.execute-api.${region}.amazonaws.com/${stage}`)
+        spinner.start('Deploying the API to a live address')
+        await amazon.deployApiGateway({ id, stage })
+        spinner.succeed(`Now serving live requests at: https://${id}.execute-api.${region}.amazonaws.com/${stage}`)
+      }
     }
+  } catch (err) {
+    spinner.fail(err.toString())
+
+    throw err
   }
 }
 
