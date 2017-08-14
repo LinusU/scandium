863d8e20158aa813165fe0ba30072008344cd094
Linus Unnebäck <linus@folkdatorn.se>
2017-08-14 23:10:59 +0100

    🌹 Move amazon code out of index.js

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 #!/usr/bin/env node
 
-const Lambda = require('aws-sdk/clients/lambda')
-const APIGateway = require('aws-sdk/clients/apigateway')
 const neodoc = require('neodoc')
 const parseArn = require('aws-arn-parser')
-const shortid = require('shortid')
 
+const amazon = require('./lib/amazon')
 const builder = require('./lib/builder')
 const swagger = require('./lib/swagger')
-
-const lambda = new Lambda({ apiVersion: '2015-03-31' })
-const apiGateway = new APIGateway({ apiVersion: '2015-07-09' })
 
 const usage = `
 Scandium
@@ -27,94 +22,12 @@ Options:
   --deploy        Deploy the API and make it callable from the Internet.
 `
 
-const defaultParams = {
-  MemorySize: 320,
-  Timeout: 3,
-  Runtime: 'nodejs6.10'
-}
-
-function createFunction ({ zipFile, functionName, role }) {
-  const params = {
-    Code: { ZipFile: zipFile },
-    FunctionName: functionName,
-    Handler: 'scandium-entrypoint.handler',
-    MemorySize: defaultParams.MemorySize,
-    Publish: true,
-    Role: role,
-    Runtime: defaultParams.Runtime,
-    Timeout: defaultParams.Timeout
-  }
-
-  return lambda.createFunction(params).promise()
-}
-
-function updateFunction ({ zipFile, functionName }) {
-  const params = {
-    FunctionName: functionName,
-    Publish: true,
-    ZipFile: zipFile
-  }
-
-  return lambda.updateFunctionCode(params).promise()
-}
-
-function addPermission ({ lambdaArn, restApiId }) {
-  const { region, namespace } = parseArn(lambdaArn)
-
-  const params = {
-    Action: 'lambda:InvokeFunction',
-    FunctionName: lambdaArn,
-    Principal: 'apigateway.amazonaws.com',
-    StatementId: `${shortid.generate()}-AllowExecutionFromAPIGateway`,
-    SourceArn: `arn:aws:execute-api:${region}:${namespace}:${restApiId}/*/*/*`
-  }
-
-  return lambda.addPermission(params).promise()
-}
-
-async function createApiGateway ({ definition, lambdaArn }) {
-  const params = {
-    body: JSON.stringify(definition),
-    failOnWarnings: true
-  }
-
-  const result = await apiGateway.importRestApi(params).promise()
-
-  await addPermission({ lambdaArn, restApiId: result.id })
-
-  return result
-}
-
-async function updateApiGateway ({ id, definition, lambdaArn }) {
-  const params = {
-    body: JSON.stringify(definition),
-    restApiId: id,
-    failOnWarnings: true,
-    mode: 'overwrite'
-  }
-
-  const result = await apiGateway.putRestApi(params).promise()
-
-  await addPermission({ lambdaArn, restApiId: result.id })
-
-  return result
-}
-
-function deployApiGateway ({ id, stage }) {
-  const params = {
-    restApiId: id,
-    stageName: stage
-  }
-
-  return apiGateway.createDeployment(params).promise()
-}
-
 async function main () {
   const args = neodoc.run(usage)
 
   if (args.create) {
     const zipFile = await builder.createZipFile(process.cwd())
-    const result = await createFunction({ zipFile, functionName: args['<name>'], role: args['--role'] })
+    const result = await amazon.createFunction({ zipFile, functionName: args['<name>'], role: args['--role'] })
 
     const lambdaArn = result.FunctionArn
 
@@ -124,7 +37,7 @@ async function main () {
       ? await swagger.loadSwaggerFile(args['--swagger'], lambdaArn)
       : await swagger.forwardAllDefinition(lambdaArn)
 
-    const { id } = await createApiGateway({ definition, lambdaArn })
+    const { id } = await amazon.createApiGateway({ definition, lambdaArn })
 
     console.log(`Created new API Gateway with id: ${id}`)
 
@@ -132,7 +45,7 @@ async function main () {
       const stage = 'prod'
       const { region } = parseArn(lambdaArn)
 
-      await deployApiGateway({ id, stage })
+      await amazon.deployApiGateway({ id, stage })
 
       console.log(`Now serving live requests at: https://${id}.execute-api.${region}.amazonaws.com/${stage}`)
     }
@@ -140,7 +53,7 @@ async function main () {
 
   if (args.update) {
     const zipFile = await builder.createZipFile(process.cwd())
-    const result = await updateFunction({ zipFile, functionName: args['<name>'] })
+    const result = await amazon.updateFunction({ zipFile, functionName: args['<name>'] })
 
     const lambdaArn = result.FunctionArn
 
@@ -152,7 +65,7 @@ async function main () {
 
     const id = args['--rest-api-id']
 
-    await updateApiGateway({ id, definition, lambdaArn })
+    await amazon.updateApiGateway({ id, definition, lambdaArn })
 
     console.log(`Updated existing API Gateway with id: ${id}`)
 
@@ -160,7 +73,7 @@ async function main () {
       const stage = 'prod'
       const { region } = parseArn(lambdaArn)
 
-      await deployApiGateway({ id, stage })
+      await amazon.deployApiGateway({ id, stage })
 
       console.log(`Now serving live requests at: https://${id}.execute-api.${region}.amazonaws.com/${stage}`)
     }
