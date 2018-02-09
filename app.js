eee743025804cf5565c878f3ba6b96b05c0aa586
Linus Unnebäck <linus@folkdatorn.se>
2018-02-09 12:40:41 +0000

    🐛 Fix "--verbose" argument parsing

--- app.js
+++ app.js
@@ -3,10 +3,11 @@
 /* Make sure that AWS uses local config files, since this is a CLI util */
 process.env.AWS_SDK_LOAD_CONFIG = 'true'
 
-const neodoc = require('neodoc')
 const awsHasRegion = require('aws-has-region')
-const Listr = require('listr')
 const isCI = require('is-ci')
+const Listr = require('listr')
+const listrVerboseRenderer = require('listr-verbose-renderer')
+const neodoc = require('neodoc')
 
 const tasks = require('./lib/tasks')
 
@@ -15,45 +16,50 @@ class UserError extends Error {}
 const usage = `
 Scandium
 
-Usage:
-  scandium create <name> [--swagger=<swagger>] [--deploy-to=<stage>] [--role=<role>]
-  scandium update <name> [--swagger=<swagger>] [--deploy-to=<stage>] --rest-api-id=<rest-api-id>
+usage:
+  scandium [global options] create <name> [--swagger=<swagger>] [--deploy-to=<stage>] [--role=<role>]
+  scandium [global options] update <name> [--swagger=<swagger>] [--deploy-to=<stage>] --rest-api-id=<rest-api-id>
 
-Options:
+options:
   <name>          Name of the Lambda function.
   --role          ARN of the IAM role that Lambda assumes when it executes your function.
   --swagger       Path to Swagger API definition used to configure AWS API Gateway.
   --rest-api-id   ID of the AWS API Gateway rest api to update (printed by the "create" command).
   --deploy-to     Deploy the API to the specified stage, and make it callable from the Internet.
-  --verbose       Use verbose output renderer.
+
+global options:
+  --help          Show this help, then exit.
+  --verbose       Print verbose output.
 `
 
-const args = neodoc.run(usage)
-
-const listrOpts = {
-  renderer: isCI || args['--verbose'] ? require('listr-verbose-renderer') : require('listr-update-renderer') 
-}
-
-const createList = new Listr([
-  tasks.packageApp,
-  tasks.createLambdaRole,
-  tasks.createLambdaFunction,
-  tasks.loadSwaggerDefinition,
-  tasks.generateSwaggerDefinition,
-  tasks.createApiGateway,
-  tasks.deployApi
-], listrOpts)
-
-const updateList = new Listr([
-  tasks.packageApp,
-  tasks.updateLambdaFunction,
-  tasks.loadSwaggerDefinition,
-  tasks.generateSwaggerDefinition,
-  tasks.updateApiGateway,
-  tasks.deployApi
-], listrOpts)
-
 async function main () {
+  const args = neodoc.run(usage, { laxPlacement: true })
+
+  const listrOpts = {}
+
+  if (isCI || args['--verbose']) {
+    listrOpts.renderer = listrVerboseRenderer
+  }
+
+  const createList = new Listr([
+    tasks.packageApp,
+    tasks.createLambdaRole,
+    tasks.createLambdaFunction,
+    tasks.loadSwaggerDefinition,
+    tasks.generateSwaggerDefinition,
+    tasks.createApiGateway,
+    tasks.deployApi
+  ], listrOpts)
+
+  const updateList = new Listr([
+    tasks.packageApp,
+    tasks.updateLambdaFunction,
+    tasks.loadSwaggerDefinition,
+    tasks.generateSwaggerDefinition,
+    tasks.updateApiGateway,
+    tasks.deployApi
+  ], listrOpts)
+
   if (!awsHasRegion()) {
     throw new UserError(awsHasRegion.errorText)
   }
